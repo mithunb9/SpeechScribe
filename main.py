299b3c168598df9391dc7ec1cde756fe11455a87
Mithun Balasubramanian <24193159+mithunb9@users.noreply.github.com>
2023-01-27 20:09:52 -0600

    add flask server

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import cohere
-import os
-from dotenv import load_dotenv
+from flask import Flask
 
-load_dotenv()
+app = Flask(__name__)
 
-co = cohere.Client(os.getenv("COHERE_API_KEY"))
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
